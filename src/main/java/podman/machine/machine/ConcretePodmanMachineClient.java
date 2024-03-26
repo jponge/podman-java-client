7f116d611541c5171f233125f02da609e57269da
Julien Ponge <jponge@redhat.com>
2024-03-26 15:24:53 +0100

    WIP

--- src/main/java/podman/machine/machine/ConcretePodmanMachineClient.java
+++ src/main/java/podman/machine/machine/ConcretePodmanMachineClient.java
@@ -5,10 +5,10 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -57,22 +57,16 @@ class ConcretePodmanMachineClient implements PodmanMachineClient {
     }
 
     private Object run(String... command) throws IOException, InterruptedException, TimeoutException {
-        Path tempFile = Files.createTempFile("podman-machine", "config");
-        Process process = new ProcessBuilder(command)
-                .redirectOutput(tempFile.toFile())
+        ProcessResult processResult = new ProcessExecutor(command)
+                .readOutput(true)
                 .redirectErrorStream(true)
-                .start();
-        if (!process.waitFor(options.getTimeout().toMillis(), TimeUnit.MILLISECONDS)) {
-            tempFile.toFile().delete();
-            throw new TimeoutException("Failed to complete operation within 30 seconds");
-        }
-        String payload = Files.readString(tempFile);
-        tempFile.toFile().delete();
-        int exitCode = process.exitValue();
-        if (exitCode == 0) {
+                .timeout(options.getTimeout().toMillis(), TimeUnit.MILLISECONDS)
+                .execute();
+        String payload = processResult.outputUTF8();
+        if (processResult.getExitValue() == 0) {
             return Json.decodeValue(payload);
         } else {
-            throw new IOException("Failed with exit code " + exitCode + "\n" + payload);
+            throw new IOException("Failed with exit code " + processResult.getExitValue() + "\n" + payload);
         }
     }
 }
